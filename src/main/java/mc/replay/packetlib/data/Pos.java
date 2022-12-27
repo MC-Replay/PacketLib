5f5a129b608dfa7afc1d0e2bc3878695dd15a26c
Itz_KiwiSap_ <programmingjesse@gmail.com>
2022-12-27 20:32:32 +0100

    Added add and subtract methods to Pos

--- src/main/java/mc/replay/packetlib/data/Pos.java
+++ src/main/java/mc/replay/packetlib/data/Pos.java
@@ -16,4 +16,24 @@ public record Pos(double x, double y, double z, float yaw, float pitch) {
     public static @NotNull Pos of(double x, double y, double z) {
         return new Pos(x, y, z, 0, 0);
     }
+
+    public @NotNull Pos add(@NotNull Pos delta) {
+        return new Pos(
+                this.x + delta.x(),
+                this.y + delta.y(),
+                this.z + delta.z(),
+                this.yaw + delta.yaw(),
+                this.pitch + delta.pitch()
+        );
+    }
+
+    public @NotNull Pos subtract(@NotNull Pos other) {
+        return new Pos(
+                this.x - other.x(),
+                this.y - other.y(),
+                this.z - other.z(),
+                this.yaw - other.yaw(),
+                this.pitch - other.pitch()
+        );
+    }
 }
