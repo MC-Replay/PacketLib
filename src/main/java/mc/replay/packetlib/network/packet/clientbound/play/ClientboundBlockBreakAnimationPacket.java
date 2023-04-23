104954a095a99b60114621908391bc7b933a60fe
Itz_KiwiSap_ <programmingjesse@gmail.com>
2023-04-23 02:18:27 +0200

    Use vector instead of pos for block position in ClientboundBlockBreakAnimationPacket

--- src/main/java/mc/replay/packetlib/network/packet/clientbound/play/ClientboundBlockBreakAnimationPacket.java
+++ src/main/java/mc/replay/packetlib/network/packet/clientbound/play/ClientboundBlockBreakAnimationPacket.java
@@ -1,41 +1,28 @@
 package mc.replay.packetlib.network.packet.clientbound.play;
 
-import mc.replay.packetlib.data.Pos;
 import mc.replay.packetlib.network.ReplayByteBuffer;
 import mc.replay.packetlib.network.packet.clientbound.ClientboundPacket;
 import mc.replay.packetlib.network.packet.clientbound.ClientboundPacketIdentifier;
+import org.bukkit.util.Vector;
 import org.jetbrains.annotations.NotNull;
 
 import static mc.replay.packetlib.network.ReplayByteBuffer.*;
 
-public record ClientboundBlockBreakAnimationPacket(int entityID, Pos position,
+public record ClientboundBlockBreakAnimationPacket(int entityId, @NotNull Vector blockPosition,
                                                    byte stage) implements ClientboundPacket {
 
     public ClientboundBlockBreakAnimationPacket(@NotNull ReplayByteBuffer reader) {
         this(
                 reader.read(VAR_INT),
-                Pos.of(
-                        reader.read(DOUBLE),
-                        reader.read(DOUBLE),
-                        reader.read(DOUBLE),
-                        reader.read(BYTE) * 360f / 256f,
-                        reader.read(BYTE) * 360f / 256f
-                ),
+                reader.read(BLOCK_POSITION),
                 reader.read(BYTE)
         );
     }
 
     @Override
     public void write(@NotNull ReplayByteBuffer writer) {
-        writer.write(VAR_INT, this.entityID);
-
-        writer.write(DOUBLE, this.position.x());
-        writer.write(DOUBLE, this.position.y());
-        writer.write(DOUBLE, this.position.z());
-
-        writer.write(BYTE, (byte) (this.position.pitch() * 256 / 360));
-        writer.write(BYTE, (byte) (this.position.yaw() * 256 / 360));
-
+        writer.write(VAR_INT, this.entityId);
+        writer.write(BLOCK_POSITION, this.blockPosition);
         writer.write(BYTE, this.stage);
     }
 
